4a56da71c2a83c0789f3d70231f5ad301660bd67
devisri0305 <161446088+devisri0305@users.noreply.github.com>
2024-10-06 17:03:54 +0530

    Day 4 opps fun

---
+++ newfile.py
@@ -0,0 +1,6 @@
+#funtion to check whether it is a even or odd number
+number=int(input("Enter a number:"))
+if number %2==0:
+    print("It is a even number");
+else:
+    print("It is a odd number");
